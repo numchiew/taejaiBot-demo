955de8eccd01b3015ddb75893afb221c12bc1e86
Matus <numchiew14@gmail.com>
2018-01-24 20:21:32 +0700

    test

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -48,11 +48,5 @@ def handle_message():
                 if messaging_event.get('message'):
                     sender_id = messaging_event['sender']['id']
                     message_text = messaging_event['message']['text']
-                    if message_text == "Generic":
-                        send_message(sender_id,'test_test')
-                    else:
-                        send_message(sender_id, message_text + " from else")
-
-
-
-
+                    send_message(sender_id,'test_test')
+    return ''
