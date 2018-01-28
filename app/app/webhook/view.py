bc4d1141a088fa5d3be3988100c9f65fa175f9ce
Matus <numchiew14@gmail.com>
2018-01-28 14:54:26 +0700

    t

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -23,12 +23,9 @@ def send_message(sender_id, message_text):
             }
         )
     )
+
     # print(r.json)
     return
-
-def testGeneric(sender_id, message_text):
-
-    send_message(sender_id, message_text)
 
 
 @webhook_blueprint.route('/', methods=['GET'], strict_slashes=False)
@@ -49,5 +46,10 @@ def handle_message():
                 if messaging_event.get('message'):
                     sender_id = messaging_event['sender']['id']
                     message_text = messaging_event['message']['text']
-                    send_message(sender_id,message_text)
+                    send_message(sender_id, message_text)
+
     return ''
+
+
+
+
