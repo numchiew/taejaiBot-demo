062b4b64b9340c51af35eaf8e1dd9a34834e783b
Matus <numchiew14@gmail.com>
2018-01-30 16:47:36 +0700

    1

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -144,6 +144,8 @@ def handle_message():
                             searchProject(sender_id, message_text,doc)
                         elif doc['chatState'] == 1:
                             searchProject(sender_id,message_text,doc)
+                        elif (message_text.find('หมา') or message_text.find('แมว')) and message_text.find('ป่วย'):
+                            send_message(sender_id, 'เทใจไม่มีโครงการเกี่ยวกับสัตว์ป่วยนะครับ รบกวนดูช่องทางอื่น')
                         else:
                             send_message(sender_id, 'ยังไม่เข้าใจอ่ะว่าหมายความว่าอะไร ตอนนี้เราทำได้แค่ค้นหาโครงการนะ')
                             user.insert({'sender_id' : sender_id, 'chatState' : chatState})
