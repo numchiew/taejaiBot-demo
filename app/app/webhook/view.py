03a2651f82322024583dbd328e08a4a9a50d0856
Matus <numchiew14@gmail.com>
2018-01-30 14:14:27 +0700

    test

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -148,7 +148,8 @@ def handle_message():
 def searchProject(sender_id, message_text):
     u = user.find_one({'sender_id' : sender_id})
     if u is None:
-        if(u['chatState'] == 0):
+        print(u)
+        if(u['chatState'] == '0'):
             send_message(sender_id, 'ต้องการค้นหาโครงการอะไรครับ')
         else:
             send_message(sender_id, 'ตรงนี้ต้องคิวรี่')
