f43d90707e0279e6c63fcec4a4551dc0993711fa
Matus <numchiew14@gmail.com>
2018-02-02 11:33:01 +0700

    testCard

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -165,7 +165,6 @@ def sendProjectCard(result, sender_id):
             }
         }
     }
-    print(messageData)
 
     r = requests.post(
         'https://graph.facebook.com/v2.6/me/messages',
@@ -181,6 +180,7 @@ def sendProjectCard(result, sender_id):
             }
         )
     )
+    print('R : ', r)
 
 
     return
