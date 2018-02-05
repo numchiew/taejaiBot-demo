a6e16723f92c4ed381815f75afac8ffdb469c6d9
Matus <numchiew14@gmail.com>
2018-02-05 16:57:41 +0700

    testSortByDate

--- app/app/webhook/view.py
+++ app/app/webhook/view.py
@@ -254,8 +254,10 @@ def searchProject(sender_id, message_text,doc):
         send_message(sender_id, 'ต้องการค้นหาโครงการอะไรครับ')
         user.insert({'sender_id' : sender_id,'sender_name':doc['sender_name'] ,'message_text' : message_text, 'chatState' : chatState})
     else:
+        a = str(datetime.now())
+        date = a[0:10]
         taejai = mongo.db.taejai
-        result = taejai.find({'name' : {'$regex': message_text} }).limit(3)
+        result = taejai.find({'name' : {'$regex': message_text}, 'end_date' : {'$gte': date} }).limit(3)
         if result.count() <= 0:
             send_message(sender_id, 'ขณะนี้ยังไม่มีชื่อโครงการที่ใกล้เคียงกับ ' + message_text + ' นะครับ')
             user.insert({'sender_id' : sender_id,'sender_name':doc['sender_name'] ,'message_text' : message_text, 'chatState' : 0})
