6e69b09c471edb590be22b8303dfef70d87d85db
Matus <numchiew14@gmail.com>
2018-02-09 14:46:29 +0700

    test

--- app/app/main.py
+++ app/app/main.py
@@ -72,7 +72,7 @@ def findProjectName():
 
     print(res)
     data = {"taejai":res}
-    return data
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
