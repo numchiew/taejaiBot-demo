cf3fa3dd1de33857be82070644f94278c1980167
Matus <numchiew14@gmail.com>
2018-02-09 12:55:04 +0700

    test

--- app/app/main.py
+++ app/app/main.py
@@ -67,7 +67,7 @@ def findProjectName():
     searchResult = taejai.find({})
     for doc in searchResult:
         res.append({"name":doc["name"]})
-    return jsonify({"project" : res})
+    return res
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
