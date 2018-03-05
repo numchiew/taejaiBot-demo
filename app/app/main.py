75d0c7c99d913692c035fae33fe0020cf8b38d43
Matus <numchiew14@gmail.com>
2018-03-05 15:46:48 +0700

    fixed

--- app/app/main.py
+++ app/app/main.py
@@ -65,7 +65,7 @@ def handle_intent():
     elif intent == 'ขอบคุณ':
         k = json.dumps({
             "fulfillmentMessages" : [{
-                "platform" : "Facebook",
+                "platform" : "FACEBOOK",
                 "type" : 1,
                 "card":{
                     "title" : "test",
